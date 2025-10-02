48cee325b90fef47f9a9ad08618dc3c29393671a
Anonimolsun <aliyevnihat.contact@gmail.com>
2025-10-02 21:53:38 +0400

    I tried to make the solution more complex and flexible by adding a DFS function to the problem!

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,20 @@ using namespace std;
 #define ll long long
 #define pb push_back
 #define all(x) x.begin(), x.end()
+int const N = 200005;
+vector<int> g[N];
+void dfs(int u, int p){
+    for(int nx: g[u]){  
+        if(nx == p) continue;
+        dfs(nx, u);
+    }
+}
 
 void solve(){
     int n;
     cin>>n;
     vector<int> g(n);
     for(int i=0; i<n; i++) cin>>g[i];
-    sort(all(g));
     for(int i=0; i<n; i++) cout<<g[i]<<" ";
 }
 
